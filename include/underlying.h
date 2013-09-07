8a759a303161b7b9d23f899954b12a638f328f34
Berkus <berkus@gmail.com>
2013-09-07 09:08:09 +0300

    Conversion to underlying type is a constexpr.

--- include/underlying.h
+++ include/underlying.h
@@ -10,6 +10,6 @@
 
 // Slightly silly enum-class-to-underlying-type converter.
 template <typename E>
-typename std::underlying_type<E>::type to_underlying(E e) {
+constexpr typename std::underlying_type<E>::type to_underlying(E e) {
     return static_cast<typename std::underlying_type<E>::type>(e);
 }
