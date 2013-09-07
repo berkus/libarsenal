34fe5a964fd5682b2fe82b08dd5a032c62a56031
Berkus <berkus@gmail.com>
2013-09-07 10:02:21 +0300

    Finish binary debug log dumper. Can be used to look at packet history.
    Formatting resembles that of 'hexdump -C'

--- tests/test_log_dump.cpp
+++ tests/test_log_dump.cpp
@@ -6,11 +6,54 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+#include <boost/format.hpp>
 #include "flurry.h"
 #include "logging.h"
 #include "byte_array_wrap.h"
 
 using namespace std;
+
+inline char printable(char c)
+{
+    if (c < 32) return '.';
+    return c;
+}
+
+void hexdump(byte_array data)
+{
+    size_t offset = 0;
+    size_t remain = data.size();
+
+    while (remain > 0)
+    {
+        cout << boost::format("%08x  ") % offset;
+        size_t stride = remain < 16 ? remain : 16;
+
+        for(int i = 0; i < stride; ++i)
+        {
+            cout << boost::format("%02x ") % (int)(unsigned char)(data[i+offset]);
+            if (i == 7)
+                cout << ' ';
+        }
+        if (stride < 16)
+        {
+            if(stride < 8)
+                cout << ' ';
+            for(int i = 0; i < 16 - stride; ++i)
+                cout << "   ";
+        }
+        cout << " |";
+        for(int i = 0; i < stride; ++i)
+        {
+            cout << boost::format("%c") % printable(data[i+offset]);
+        }
+        cout << "|" << endl;
+
+        remain -= stride;
+        offset += stride;
+    }
+    cout << boost::format("%08x") % offset << endl;
+}
 
 int main()
 {
