7e0fc4e01afd1ecf58228fb7e3b9eb4707fa59a0
Berkus <berkus@gmail.com>
2013-09-17 19:53:40 +0300

    Add make_unique<T>

---
+++ include/make_unique.h
@@ -0,0 +1,19 @@
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#pragma once
+
+#include <memory>
+
+//
+// make_unique<T> helper because libcxx still doesn't have one.
+//
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
