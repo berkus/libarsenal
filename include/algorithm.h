79cb55406fc2c4ed9ad6788e2a6d76652e9362cf
Berkus <berkus@gmail.com>
2013-09-21 22:13:52 +0300

    Add a very simple contains(Container, Key) algorithm.
    To save some typing with the map/set containers.

---
+++ include/algorithm.h
@@ -0,0 +1,19 @@
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#pragma once
+
+#include <algorithm>
+
+// Return true if container c contains key k
+// Container must be a collection with type Key for keys - @todo express constraint in code
+template <typename Key, typename Container>
+inline bool contains(Container& c, Key& k)
+{
+    return c.find(k) != end(c);
+}
