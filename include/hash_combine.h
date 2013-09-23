df9c0a8c5edc4a137db6f99304f4db9303a440fd
Berkus <berkus@gmail.com>
2013-09-23 10:46:40 +0300

    Add hash_combine<T>.

---
+++ include/hash_combine.h
@@ -0,0 +1,24 @@
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#pragma once
+
+namespace stdext {
+
+//
+// Implementation for hash_combine missing from boost/STL/C++11.
+// Not needed once C++14 is out, hopefully.
+//
+template <class T>
+inline void hash_combine(std::size_t& seed, const T& v)
+{
+    std::hash<T> hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+}
+
+} // stdext namespace
