7d3745a78dff18c2cfb89a18a4ffa7f4e89d6a1b
Berkus <berkus@gmail.com>
2013-10-09 13:13:50 +0300

    Implement settings provider via binary plist file.

--- lib/settings_provider.cpp
+++ lib/settings_provider.cpp
@@ -1,10 +1,15 @@
+#include <boost/any.hpp>
+#include <string>
 #include "settings_provider.h"
 #include "Plist.hpp"
+
+using namespace std;
 
 namespace {
 static std::string organization_name{"defaultOrg"};
 static std::string organization_domain{"defaultDomain"};
 static std::string application_name{"defaultApp"};
+static map<string, boost::any> data;
 } // anonymous namespace
 
 void settings_provider::set_organization_name(std::string const& org_name)
@@ -29,7 +34,9 @@ std::shared_ptr<settings_provider> settings_provider::instance()
 
 settings_provider::settings_provider(settings_provider::private_tag)
 {
-
+    std::ifstream stream("someFileName.plist", std::ios::binary);
+    if(stream)
+        Plist::readPlist(stream, data);
 }
 
 bool settings_provider::enter_section(std::string const& name)
@@ -41,33 +48,45 @@ void settings_provider::leave_section()
 {}
 
 void settings_provider::set(std::string const& key, std::string const& value)
-{}
+{
+    data[key] = value;
+    Plist::writePlistBinary("someFileName.plist", data);
+}
 
 void settings_provider::set(std::string const& key, byte_array const& value)
-{}
+{
+    data[key] = value;
+    Plist::writePlistBinary("someFileName.plist", data);
+}
 
 void settings_provider::set(std::string const& key, size_t value)
-{}
+{
+    data[key] = value;
+    Plist::writePlistBinary("someFileName.plist", data);
+}
 
 void settings_provider::set(std::string const& key, ssize_t value)
-{}
+{
+    data[key] = value;
+    Plist::writePlistBinary("someFileName.plist", data);
+}
 
 std::string settings_provider::get_string(std::string const& key)
 {
-    return "";
+    return boost::any_cast<std::string>(data[key]);
 }
 
 byte_array settings_provider::get_byte_array(std::string const& key)
 {
-    return byte_array();
+    return byte_array();//boost::any_cast<std::vector>(data[key]);
 }
 
 size_t settings_provider::get_uint(std::string const& key)
 {
-    return 0;
+    return boost::any_cast<size_t>(data[key]);
 }
 
 ssize_t settings_provider::get_int(std::string const& key)
 {
-    return 0;
+    return boost::any_cast<ssize_t>(data[key]);
 }
