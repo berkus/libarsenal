d815397d8e810be4392f6797678e0c02d61eed42
Berkus <berkus@gmail.com>
2013-10-10 17:51:31 +0300

    Add common Travis builder scripts.

---
+++ travis-ci/travis_install_packages.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+# Install missing or update outdated Ubuntu packages for Travis VM.
+sudo add-apt-repository -y ppa:h-rayflood/llvm
+sudo add-apt-repository -y ppa:28msec/utils
+sudo apt-get update
+sudo apt-get install -qq --fix-missing libpulse-dev clang-3.3 cmake
