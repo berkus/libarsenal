7b1f60b195504e2026639f641102066180ccd04b
Berkus <berkus@gmail.com>
2013-10-20 15:35:34 +0300

    Add headings.

--- include/binary_literal.h
+++ include/binary_literal.h
@@ -1,4 +1,12 @@
 //
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2013, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//
 // Binary literals as an UDL.
 // 1110_b or 1010101_b etc
 // In C++14 use 0b010101 instead!
