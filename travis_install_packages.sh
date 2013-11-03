17d18d650346dd744831a33aa329b7cdf31dfe97
Berkus <berkus@gmail.com>
2013-11-03 12:51:33 +0200

    Install various missing packages by default.

--- travis_install_packages.sh
+++ travis_install_packages.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 # Install missing or update outdated Ubuntu packages for Travis VM.
+sudo apt-get install -qq python-software-properties # add-apt-repository
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
 sudo add-apt-repository -y ppa:28msec/utils
 sudo apt-get update
-sudo apt-get install -qq --fix-missing libpulse-dev clang-3.4 cmake
+sudo apt-get install -qq --fix-missing libpulse-dev clang-3.4 cmake libssl-dev
 
 # Don't waste time installing wrong boost...
 # libboost-all-dev
