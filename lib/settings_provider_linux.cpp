a7e02152a773ec8fa249117df611b00b7e73a65d
Berkus <berkus@gmail.com>
2013-11-22 14:12:52 +0200

    Create parent directories for settings provider on Linux.
    Adds dependency on boost.filesystem.

--- lib/settings_provider_linux.cpp
+++ lib/settings_provider_linux.cpp
@@ -2,10 +2,20 @@
 // Save config to ~/.config/<orgname>/appname.plist
 #include <string>
 #include <sstream>
+#include <boost/filesystem.hpp>
+#include "logging.h"
+
+namespace fs = boost::filesystem;
 
 std::string settings_file_name(std::string orgname, std::string orgdomain, std::string appname)
 {
     std::ostringstream os;
-    os << getenv("HOME") << "/.config/" << orgname << "/" << appname << ".plist";
+    os << getenv("HOME") << "/.config/" << orgname;
+    try {
+        fs::create_directories(os.str());
+    } catch(std::system_error& e) {
+        logger::warning() << "Cannot create config file directory " << os.str() << " - " << e.what();
+    }
+    os << "/" << appname << ".plist";
     return os.str();
 }
