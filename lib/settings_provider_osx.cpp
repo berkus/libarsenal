35d4f6c5b6bc0f01cb8a2237fc1909fddd81ccab
Berkus <berkus@gmail.com>
2014-01-06 02:31:10 +0200

    OSX config file is now not a plist.

--- lib/settings_provider_osx.cpp
+++ lib/settings_provider_osx.cpp
@@ -7,6 +7,6 @@ std::string settings_file_name(std::string orgname, std::string orgdomain, std::
 {
     std::ostringstream os;
     os << getenv("HOME") << "/Library/Preferences/";
-    os << orgdomain << "." << appname << ".plist";
+    os << orgdomain << "." << appname << ".config";
     return os.str();
 }
