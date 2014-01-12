41aa0831969da29c0ee499f0c2dbf36ecb7689e4
Berkus <berkus@gmail.com>
2014-01-12 10:07:22 +0200

    python-software-properties is not found in ubuntu repos?

--- travis_install_packages.sh
+++ travis_install_packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # Install missing or update outdated Ubuntu packages for Travis VM.
-sudo apt-get install -qq python-software-properties # add-apt-repository
+# sudo apt-get install -qq python-software-properties # add-apt-repository
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
 sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
