925999efa99adb456975cd4aef6e723ff8ab7631
Berkus <berkus@gmail.com>
2014-01-13 18:31:08 +0200

    Added apt-get clean as per ticket suggestion https://github.com/travis-ci/travis-ci/issues/1854

--- travis_install_packages.sh
+++ travis_install_packages.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 # Install missing or update outdated Ubuntu packages for Travis VM.
-#dpkg --get-selections | grep hold
-
-#wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
-#sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
-#sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
+wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
+sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
+sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
+sudo apt-get clean
 sudo apt-get update
 sudo apt-get install -qq --fix-missing libpulse-dev clang-3.5 cmake libssl-dev
