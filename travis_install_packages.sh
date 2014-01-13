be0bdf569b4f2b406e0479223c40c61f44313633
Berkus <berkus@gmail.com>
2014-01-13 17:21:45 +0200

    Disable some package source commands in order to debug install failure.

--- travis_install_packages.sh
+++ travis_install_packages.sh
@@ -1,24 +1,7 @@
 #!/usr/bin/env sh
 # Install missing or update outdated Ubuntu packages for Travis VM.
-# sudo apt-get install -qq python-software-properties # add-apt-repository
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
-sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
+#sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
 sudo apt-get update
 sudo apt-get install -qq --fix-missing libpulse-dev clang-3.5 cmake libssl-dev
-
-# Don't waste time installing wrong boost...
-# libboost-all-dev
-
-# which clang
-# /usr/bin/clang --version
-
-# Default clang installation is in /usr/local/bin/clang
-#
-# clang-3.4 installs the following binaries:
-# /usr/bin/clang
-# /usr/bin/clang-check
-# /usr/bin/clang++
-# /usr/bin/pollycc
-# /usr/bin/asan_symbolize
-# /usr/bin/scan-build
