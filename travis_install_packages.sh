4187ef8386c161d32f48f2fcb3887a0609ef3b32
Berkus <berkus@gmail.com>
2014-01-13 17:26:22 +0200

    Display help packages. (debug)

--- travis_install_packages.sh
+++ travis_install_packages.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 # Install missing or update outdated Ubuntu packages for Travis VM.
+dpkg --get-selections | grep hold
+
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
 #sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
