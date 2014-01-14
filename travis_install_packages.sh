ea69ded2b9331451a301398c2daa2e81ccce9766
Berkus <berkus@gmail.com>
2014-01-14 11:32:00 +0200

    Add gcc-4.8 backport to try to fix dependency problems in https://github.com/travis-ci/travis-ci/issues/1854

--- travis_install_packages.sh
+++ travis_install_packages.sh
@@ -3,6 +3,7 @@
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
 sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/ppa # gcc-4.8 backport for clang-3.5
 sudo apt-get clean
 sudo apt-get update
 sudo apt-get install -qq --fix-missing libpulse-dev clang-3.5 cmake libssl-dev
