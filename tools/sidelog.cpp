d1c6753ec36756fe64024e2f0b33095890c662e4
Berkus <berkus@gmail.com>
2014-06-08 13:24:07 +0300

    Fix order in typedef.

--- tools/sidelog.cpp
+++ tools/sidelog.cpp
@@ -63,7 +63,7 @@ public:
 
 class log_input : public file_input
 {
-    typedef super file_input;
+    typedef file_input super;
 
 public:
     log_input(string filename, string name, int indent, int width_limit)
@@ -100,7 +100,7 @@ public:
 
 class bin_input : public file_input
 {
-    typedef super file_input;
+    typedef file_input super;
     flurry::iarchive ia_;
 
 public:
