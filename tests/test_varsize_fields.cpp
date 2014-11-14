c8d1c51865889ec2a72702d35eadf0e477b0a4a5
Berkus Decker <berkus@madfire.net>
2014-11-14 19:34:19 +0200

    Varsize field reader works. Needs more improvements though.

--- tests/test_varsize_fields.cpp
+++ tests/test_varsize_fields.cpp
@@ -1,5 +1,5 @@
 // Types needed:
-// * buffer that runs from a point until the end of asio::buffer
+// * buffer that runs from current point until the end of asio::buffer
 //   (should be easy - when we see it in types, just consume the rest of the buffer)
 //   sentinel type rest_t to grab until the rest of buffer... must be last in struct
 // * a variable sized field, where size is controlled by external bitfield with certain mapping
@@ -10,6 +10,7 @@
 #include <array>
 #include <iostream>
 #include <boost/asio/buffer.hpp>
+#include <boost/optional.hpp>
 #include <boost/mpl/range_c.hpp>
 #include <boost/mpl/for_each.hpp>
 #include <boost/mpl/size.hpp>
@@ -22,31 +23,36 @@
 #define BOOST_TEST_MODULE Test_varsize_fields
 #include <boost/test/unit_test.hpp>
 
+//=================================================================================================
+// Templated part
+//=================================================================================================
+
 using namespace std;
+using boost::optional;
 namespace asio = boost::asio;
 namespace mpl = boost::mpl;
 namespace fusion = boost::fusion;
 
-// template <typename T, size_t M, size_t O>
-// struct varsize_field
-// {
-//     T value;
-//     constexpr static const size_t bit_mask = M;
-//     constexpr static const size_t bit_mask_offset = O;
-// };
+constexpr unsigned int operator"" _bits_mask (unsigned long long bits)
+{
+    return (1 << bits) - 1;
+}
 
 // Optional bitmask position
-// template <typename T, size_t N = CHAR_BIT * sizeof(T)>
-// struct optional_field_set
-// {
-//     using value_type = T;
-//     using bits_type = std::bitset<N>;
-// };
+template <typename T, size_t N = CHAR_BIT * sizeof(T)>
+struct varsize_field_flag
+{
+    using value_type = T;
+    using bits_type = std::bitset<N>;
+};
 
-std::array<uint8_t,18> buffer  = {{ 0x00,
-                                    0x01, 0xab, 0xcd,
-                                    0x02, 0xab, 0xcd, 0xef, 0x12,
-                                    0x03, 0xab, 0xcd, 0xef, 0x12, 0x34, 0x56, 0x78, 0x9a }};
+template <typename T, size_t M, size_t O>
+struct varsize_field_specification
+{
+    T value; // varsized_field_wrapper
+    constexpr static const size_t bit_mask = M;
+    constexpr static const size_t bit_mask_offset = O;
+};
 
 struct nothing_t
 {
@@ -54,41 +60,9 @@ struct nothing_t
     operator T() { return T(); } // convert to anything - should actually return empty optional?
 };
 
-struct reader
-{
-    mutable asio::const_buffer buf_;
-
-    explicit reader(asio::const_buffer b) : buf_(std::move(b)) {}
-
-    template <class T>
-    auto operator()(T& val) const -> typename std::enable_if<std::is_integral<T>::value>::type
-    {
-        val = *asio::buffer_cast<T const*>(buf_);
-        buf_ = buf_ + sizeof(T);
-    }
-
-    void operator()(nothing_t& val) const
-    {
-        // Do nothing!
-    }
-};
-
-// User-defined mapping function (switcher bits value to type)
-// based on the value of some field we must choose N-th value in this struct and read it
-// index 0 - bits value 0, index 1 - bits value 1 and so on
-// use a sentinel type nothing_t to read nothing.
-// @todo When nothing_t is present we should be using optional<V> for output value?
-BOOST_FUSION_DEFINE_STRUCT(
-    (mapping), twobits,
-    (nothing_t, no_value)
-    (uint16_t, value1)
-    (uint32_t, value2)
-    (uint64_t, value3)
-);
-
 // from above switcher struct we need to copy the appropriate field into output struct:
 template <typename SwitchType, typename FinalType>
-struct varsized_field_wrapper
+struct varsize_field_wrapper
 {
     SwitchType choice_;
     FinalType output_;
@@ -99,6 +73,8 @@ struct varsized_field_wrapper
 template <class T>
 using range_c = typename mpl::range_c<int, 0, mpl::size<T>::value>;
 
+struct reader;
+
 struct read_fields
 {
     template <typename T, typename V>
@@ -106,10 +82,10 @@ struct read_fields
     {
         T& output_;
         V& result_;
-        reader& read_;
+        reader const& read_;
         uint8_t value_;
 
-        read_field_visitor(T& out, V& result, reader& r, uint8_t val)
+        read_field_visitor(T& out, V& result, reader const& r, uint8_t val)
             : output_(out)
             , result_(result)
             , read_(r)
@@ -127,50 +103,111 @@ struct read_fields
     };
 
     template <typename T, typename V>
-    void operator()(varsized_field_wrapper<T,V>& w, reader& r, uint8_t value)
+    void operator()(varsize_field_wrapper<T,V>& w, reader const& r, uint8_t value)
     {
         mpl::for_each<range_c<T>>(read_field_visitor<T,V>(w.choice_, w.output_, r, value));
     }
 };
 
+struct reader
+{
+    mutable asio::const_buffer buf_;
+    // temp until i figure how to pass this state:
+    mutable optional<varsize_field_flag<uint8_t>::bits_type> vars_;
+
+    explicit reader(asio::const_buffer b) : buf_(std::move(b)) {}
+
+    template <class T>
+    auto operator()(T& val) const -> typename std::enable_if<std::is_integral<T>::value>::type
+    {
+        val = *asio::buffer_cast<T const*>(buf_);
+        buf_ = buf_ + sizeof(T);
+    }
+
+    void operator()(varsize_field_flag<uint8_t>) const { // Temp: this also depends on type of parsed struct
+        varsize_field_flag<uint8_t>::value_type val;
+        (*this)(val);
+        vars_ = varsize_field_flag<uint8_t>::bits_type(val);
+    }
+
+    template <typename T, size_t M, size_t O>
+    void operator()(varsize_field_specification<T,M,O>& val) const
+    {
+        if (!vars_) {
+            throw logic_error("Variable value comes before the defining flags");
+        }
+        varsize_field_flag<uint8_t>::value_type vflag = vars_->to_ulong();
+        vflag = (vflag >> O) & M;
+        (*this)(val.value, vflag);
+    }
+
+    template <typename T, typename V>
+    void operator()(varsize_field_wrapper<T,V>& val, varsize_field_flag<uint8_t>::value_type flag_value) const // @todo
+    {
+        read_fields read_field_;
+        read_field_(val, *this, flag_value);
+    }
+
+    template <class T>
+    auto operator()(T & val) const ->
+        typename std::enable_if<boost::fusion::traits::is_sequence<T>::value>::type
+    {
+        boost::fusion::for_each(val, *this);
+    }
+
+    void operator()(nothing_t&) const
+    {
+        // Do nothing!
+    }
+};
+
+//=================================================================================================
+// User-defined part
+//=================================================================================================
+
+// User-defined mapping function (switcher bits value to type)
+// based on the value of some field we must choose N-th value in this struct and read it
+// index 0 - bits value 0, index 1 - bits value 1 and so on
+// use a sentinel type nothing_t to read nothing.
+// @todo When nothing_t is present we should be using optional<V> for output value?
+BOOST_FUSION_DEFINE_STRUCT(
+    (mapping), twobits,
+    (nothing_t, no_value)
+    (uint16_t, value1)
+    (uint32_t, value2)
+    (uint64_t, value3)
+);
+
+using flag_field_t = varsize_field_flag<uint8_t>;
+using packet_size_t = varsize_field_wrapper<mapping::twobits, uint64_t>;
+using packet_field_t = varsize_field_specification<packet_size_t, 2_bits_mask, 2>;
+
+BOOST_FUSION_DEFINE_STRUCT(
+    (actual), header_type,
+    (flag_field_t, flags)
+    (packet_field_t, packet_size)
+);
+
+std::array<uint8_t,18> buffer  = {{ 0x00,
+                                    0x04, 0xab, 0xcd,
+                                    0x08, 0xab, 0xcd, 0xef, 0x12,
+                                    0x0c, 0xab, 0xcd, 0xef, 0x12, 0x34, 0x56, 0x78, 0x9a }};
+
 BOOST_AUTO_TEST_CASE(basic_reader)
 {
     asio::const_buffer buf(buffer.data(), buffer.size());
-    reader r(std::move(buf));
-    read_fields rf;
+    reader read_(std::move(buf));
+    actual::header_type header;
 
-    uint8_t flags;
-    varsized_field_wrapper<mapping::twobits, uint64_t> packet_size;
+    read_(header);
+    BOOST_CHECK(header.packet_size.value.value() == 0);
 
-    r(flags);
-    rf(packet_size, r, flags);
+    read_(header);
+    BOOST_CHECK(header.packet_size.value.value() == 0xcdab);
 
-    BOOST_CHECK(flags == 0);
-    BOOST_CHECK(packet_size.value() == 0);
+    read_(header);
+    BOOST_CHECK(header.packet_size.value.value() == 0x12efcdab);
 
-    cout << (int)flags << " / " << packet_size.value() << endl;
-
-    r(flags);
-    rf(packet_size, r, flags);
-
-    BOOST_CHECK(flags == 1);
-    BOOST_CHECK(packet_size.value() == 0xcdab);
-
-    cout << (int)flags << " / " << packet_size.value() << endl;
-
-    r(flags);
-    rf(packet_size, r, flags);
-
-    BOOST_CHECK(flags == 2);
-    BOOST_CHECK(packet_size.value() == 0x12efcdab);
-
-    cout << (int)flags << " / " << packet_size.value() << endl;
-
-    r(flags);
-    rf(packet_size, r, flags);
-
-    BOOST_CHECK(flags == 3);
-    BOOST_CHECK(packet_size.value() == 0x9a78563412efcdab);
-
-    cout << (int)flags << " / " << packet_size.value() << endl;
+    read_(header);
+    BOOST_CHECK(header.packet_size.value.value() == 0x9a78563412efcdab);
 }
