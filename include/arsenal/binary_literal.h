ef7ab4fea4ae9d9fdf4e6b5421bcf0e0074b2554
Berkus Decker <berkus@madfire.net>
2017-05-16 20:18:01 +0300

    Disable obsolete binary literals

--- include/arsenal/binary_literal.h
+++ include/arsenal/binary_literal.h
@@ -9,19 +9,5 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-#pragma once
 
-template< char FIRST, char... REST > struct binary
-{
-    static_assert( FIRST == '0' || FIRST == '1', "invalid binary digit" ) ;
-    enum { value = ( ( FIRST - '0' ) << sizeof...(REST) ) + binary<REST...>::value  } ;
-};
-
-template<> struct binary<'0'> { enum { value = 0 } ; };
-template<> struct binary<'1'> { enum { value = 1 } ; };
-
-template<  char... LITERAL > inline
-constexpr unsigned int operator "" _b() { return binary<LITERAL...>::value ; }
-
-template<  char... LITERAL > inline
-constexpr unsigned int operator "" _B() { return binary<LITERAL...>::value ; }
+#error Use 0b010101 notation!
