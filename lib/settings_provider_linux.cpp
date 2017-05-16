81e04ed5a1c1c16d78c4f5c504d489f201ea4ceb
Berkus Decker <berkus@madfire.net>
2017-05-17 00:48:10 +0300

    Use std::filesystem

--- lib/settings_provider_linux.cpp
+++ lib/settings_provider_linux.cpp
@@ -5,22 +5,21 @@
 #include <boost/filesystem.hpp>
 #include <boost/log/trivial.hpp>
 
-namespace fs = boost::filesystem;
+namespace fs = std::filesystem;
 
 namespace arsenal::detail
 {
 
 std::string settings_file_name(std::string orgname, std::string orgdomain, std::string appname)
 {
-    std::ostringstream os;
-    os << getenv("HOME") << "/.config/" << orgname;
+    fs::path p = getenv("HOME") / ".config" / orgname;
     try {
-        fs::create_directories(os.str());
-    } catch(std::system_error& e) {
+        fs::create_directories(p);
+    } catch(std::filesystem_error const& e) {
         BOOST_LOG_TRIVIAL(warning) << "Cannot create config file directory " << os.str() << " - " << e.what();
     }
-    os << "/" << appname << ".config";
-    return os.str();
+    p /= appname + ".config";
+    return p.native();
 }
 
 } // arsenal::detail namespace
--- lib/settings_provider_osx.cpp
+++ lib/settings_provider_osx.cpp
@@ -2,16 +2,16 @@
 // Save config to ~/Library/Preferences/<orgdomain>.<appname>.config
 #include <string>
 #include <sstream>
+#include <filesystem>
 
 namespace arsenal::detail
 {
 
 std::string settings_file_name(std::string orgname, std::string orgdomain, std::string appname)
 {
-    std::ostringstream os;
-    os << getenv("HOME") << "/Library/Preferences/";
-    os << orgdomain << "." << appname << ".config";
-    return os.str();
+	std::filesystem::path p = getenv("HOME") / "Library" / "Preferences";
+	p =/ orgdomain + "." + appname + ".config";
+	return p.native();
 }
 
 } // arsenal::detail namespace
